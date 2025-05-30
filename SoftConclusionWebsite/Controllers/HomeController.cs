d2f9fe7197e5c38d29c6466d59273c5b36f463a7
Hani M. Almutlaq <115640689+Hani-Almutlaq@users.noreply.github.com>
2025-05-30 16:59:24 +0300

    Add files via upload

---
+++ SoftConclusionWebsite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SoftConclusionWebsite.Models;
+
+namespace SoftConclusionWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MindMaps()
+        {
+            return View();
+        }
+
+        public IActionResult Library()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
